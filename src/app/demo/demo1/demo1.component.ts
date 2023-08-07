685d4a0302cd259611f7982a22dc7ceb85e7a9eb
Aude Beurive <aude.beurive@bstorm.be>
2023-08-07 12:18:53 +0200

    demo bindings

--- src/app/demo/demo1/demo1.component.ts
+++ src/app/demo/demo1/demo1.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo1.component.scss']
 })
 export class Demo1Component {
+  //Property Binding
+  prenom : string = 'Aude';
+  nom : string = 'Beurive';
 
+  //Attribute Binding
+  monImage : string = "https://imgflip.com/s/meme/Cute-Cat.jpg";
+  isNotAvailable : boolean = true;
+
+  //Event Binding
+  message : string = "...[En attente d'une salutation]...";
+
+  direBonjour(langue : string) : void {
+    switch(langue) {
+      case 'fr' :
+        this.message = "Wesh zalu";
+        break;
+      case 'en' : 
+        this.message = "Hi dude";
+        break;
+      case 'it' :
+        this.message = "Ciao bella";
+        break;
+      default :
+        this.message = "G pa compri";
+        break;
+    }
+  }
 }
