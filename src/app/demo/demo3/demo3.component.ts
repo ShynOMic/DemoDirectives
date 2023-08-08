16a79af3246ba3e4a3601523b8c6cf89ad883d34
Aude Beurive <aude.beurive@bstorm.be>
2023-08-08 12:18:38 +0200

    demo directives components & structurals

--- src/app/demo/demo3/demo3.component.ts
+++ src/app/demo/demo3/demo3.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Trainer } from 'src/app/shared/models/trainers';
 
 @Component({
   selector: 'app-demo3',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo3.component.scss']
 })
 export class Demo3Component {
+  //#region Components
+  //ngStyle
+  favColor : string = "#7FFF00";
+  favColor2 : string = "darkblue";
+  width : number = 0;
+  timer : any = undefined;
+
+  fill() : void {
+    this.timer = setInterval(() => {
+      if(this.width < 100) {
+        this.width+=10;
+      }
+    }, 100)
+  }
+
+  empty() : void {
+    clearInterval(this.timer);
+    this.timer = undefined;
+    this.width = 0;
+  }
+
+  //ngClass
+  isBig : boolean = false;
+  isPink : boolean = false;
+  isItalic : boolean = false;
+
+  //#endregion
+
+  //#region Structural
+  //ngIf
+  isDay : boolean = true;
+
+  //ngFor
+  trainersNameList : string[] = ['Khun', 'Gavin', 'Aude', 'Aurélien'];
+
+  trainersList : Trainer[] = [
+    { id : 0, firstname : 'Khun', lastname : 'Ly', birthdate : new Date(1982,4,6), gender : 'm', avatar : 'assets/images/khun.png' },
+
+    { id : 1, firstname : 'Aude', lastname : 'Beurive', birthdate : new Date(1989,9,16), gender : 'f', avatar : 'assets/images/aude.png' },
+
+    { id : 2, firstname : 'Gavin', lastname : 'Chaineux', birthdate : new Date(1996,9,18), gender : 'nb', avatar : 'assets/images/gavin.png' }
+  ];
+
+  //ngSwitch
+  meteo : string|undefined = undefined;
+
+  //#endregion
 
 }
