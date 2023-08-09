b68a0b1a092761a273bd98510e7e13b193ec679a
Aude Beurive <aude.beurive@bstorm.be>
2023-08-09 11:18:17 +0200

    demo base input & output

--- src/app/demo/demo4/enfant/enfant.component.ts
+++ src/app/demo/demo4/enfant/enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-enfant',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./enfant.component.scss']
 })
 export class EnfantComponent {
+    @Input() prenomParent : string = '[Pas de prénom]';
+    @Input() hobbiesParent : string[] = [];
 
+    @Output() onNeedSelect : EventEmitter<string>;
+
+    constructor() {
+      this.onNeedSelect = new EventEmitter<string>(); 
+      //instanciation de EventEmitter
+    }
+
+    sendNeed(besoin : string) {
+      this.onNeedSelect.emit(besoin);
+    }
 }
