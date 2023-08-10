2034e9dc672f9bbb0f4e14719dceed09c23b0d69
Aude Beurive <aude.beurive@bstorm.be>
2023-08-10 16:42:57 +0200

    modification custome directive ajout input

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -1,16 +1,18 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
+  @Input() bgColor : string = 'darkblue';
+  @Input() color : string = 'chartreuse';
 
   // ElementRef -> Pour récupérer l'élément sur lequel est la directive
   constructor(private _elem : ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this._elem.nativeElement.style.backgroundColor = "darkblue";
-    this._elem.nativeElement.style.color = "chartreuse";
+    this._elem.nativeElement.style.backgroundColor = this.bgColor;
+    this._elem.nativeElement.style.color = this.color;
 
   }
 
