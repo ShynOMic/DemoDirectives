5390c9bc5894fdea6be974dad9da498fef924a9e
Aude Beurive <aude.beurive@bstorm.be>
2023-08-11 10:32:34 +0200

    mise en place formulaire

---
+++ src/app/demo/demo6/demo6.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo6',
+  templateUrl: './demo6.component.html',
+  styleUrls: ['./demo6.component.scss']
+})
+export class Demo6Component {
+
+  //nom
+  //prenom
+  //birthdate
+  //email
+  //password
+  //gender
+  //single
+  //nationality
+
+  constructor() {
+    
+  }
+
+  createUser() {
+    
+  }
+}
